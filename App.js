02cc043eddf0c861d55d3e53f4e4b95a789816c0
renandasinfo <renanscaraujo2010@outlook.com>
2020-09-22 18:52:42 -0300

    Nova versão

--- App.js
+++ App.js
@@ -1,57 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
 
 import React from 'react';
-import CheckBox from '@react-native-community/checkbox';
-import {StyleSheet, Text, View,Button,TouchableOpacity,TextInput} from 'react-native';
+import {StyleSheet, Text, View,TextInput,Button} from 'react-native';
+import { Picker } from 'react-native';
+import Calculadora from 'react-native-vector-icons/FontAwesome';               
 
 
+export  default function  App () {
+ const [peso,setPeso] = React.useState(0.00);
+ const [altura,setAltura] = React.useState(0.00);
+ const [imc,setImc] = React.useState(0.00);
+ const [linguagem,setLinguagem] = React.useState();
+ console.log('valor armazenado',linguagem)
 
-
-
-
-export default function  App () {
-
-  
-  
+function calculaImc(peso,altura){
+  var resultado = peso / (altura * altura)
+  setImc(resultado)
+  console.log('valor do imc:',imc)
+  console.log('valor de altura',altura)
+  console.log('valor de peso',peso)
+}
 
   return (
-    <View style={[styles.child1]}>
-    <Text style={{ fontSize: 20,fontWeight:'bold', color:"#333333"}}>                   Calculadora de imc </Text>
-    <View style={[styles.views]}>
-    <Text>{'\n'}</Text>
-    <TextInput style={{ fontSize: 15, color:"#333333"}}>         Qual seu peso: </TextInput>
-    <TextInput style={{ fontSize: 15, color:"#333333"}}>         Qual sua altura: </TextInput>
-    <CheckBox checar = {false}
-    onPress={() => this.onPress(checar)}
-    />
-    <Text>Criança</Text>
-    <CheckBox checar= {false}
-    onPress={() => this.onPress(checar)}
-    />
-    <Text>Adulto</Text>
-
-    <CheckBox checar= {false}
-    onPress={() => this.onPress(checar)}
-    />
-    <Text>Idoso</Text>
-      </View>
+    <View style={styles.views}>
+    <Text style={{ fontSize: 20,fontWeight:'bold', color:"black"}}>           Calculadora de imc </Text>
+    <Calculadora name="calculadora" size={40} color="#FFF"></Calculadora>
       
-      <Text>{'\n'}</Text>
-      <Text>{'\n'}</Text>
-    <Text style={{ fontSize: 15,height:100, color:"#333333"}}>                             Resultado IMC: </Text>
-    
-    
-    <Text style={{ fontSize: 15,height:100, color:"#333333"}}>                             Resultado IMC: </Text>
-    
+    <Text>{'\n'}</Text>
+    <TextInput style={{ fontSize: 15, color:"#333333",borderRadius:1}}
+    placeholder="Seu peso:"
+    onChangeText={valor => setPeso(valor)}
+    value={peso}
 
-    <View style={[styles.child2]}>
-     <TouchableOpacity style={styles.Botao}>
-      <Text style={[]}> Calcular peso</Text>
-     </TouchableOpacity>
-                
-            </View>
-    </View>
     
+    ></TextInput>
+    <TextInput style={{ fontSize: 15, color:"#333333"}}
+     placeholder="Sua altura:"
+     onChangeText={(valor) => setAltura(valor)}
+     value={altura}
+    ></TextInput>
+      <Text>{'\n'}</Text>
+    <Picker
+    selectedValue={linguagem}
+    style={{height: 50, width: 100}}
+    onValueChange={(itemValue) =>
+    setLinguagem({linguagem: itemValue})
+    
+}>
+<Picker.Item label="Criança" value="Criança" />
+<Picker.Item label="Adulto" value="Adulto" />
+<Picker.Item label="Idoso" value="Idoso" />
+
+
+    </Picker>
+    <Text>{'\n'}</Text>
+    <View style={styles.botaoCalcular}>
+    <Button title="calcular" color="#333333" backgroundColor="blue" onPress={() => calculaImc(peso,altura)}></Button>
+    <Text>{'\n'}</Text>
+    </View>
+    <Text>{'\n'}</Text>
+    
+  <Text>Peso: {peso} Altura: {altura} IMC: {imc.toFixed(2)} </Text>
+
+  
+
+            </View>
+    
+     
   );
 }
 
@@ -88,13 +103,10 @@ const styles = StyleSheet.create({
 
   }, 
    Botao: {
-    backgroundColor:'#FAFAFA',
-    paddingTop: 8,
-    width: 200,
-    height: 50,
-    borderRadius:40,
-    marginTop:30,
-
+    padding: 12,
+    backgroundColor: '#DEDEDE',
+    alignContent: 'center',
+    borderRadius: 50,
   },
   views2: {
     width: 100,
@@ -120,4 +132,16 @@ const styles = StyleSheet.create({
     margin: 20,
     
   },
+  imc:{
+    alignSelf:'center',
+    fontSize:100
+  },
+  botaoCalcular:{
+  backgroundColor:'lightgray',
+  elevation: 10,
+  borderRadius:10,
+  width: 110,
+  height: 45,
+  alignItems: "center"
+  }
   });
